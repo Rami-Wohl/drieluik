f7c735bbb538282ee042945c1a30140e2493445a
Rami Wohl <rami.wohl@nos.nl>
2024-11-06 22:27:53 +0100

    add balansspel

--- src/components/sections/zingeving/balans.tsx
+++ src/components/sections/zingeving/balans.tsx
@@ -15,214 +15,113 @@ export function Balans() {
         icon={<BalansIcon height={72} width={72} />}
         title="Balansspel"
       />
-      <DialogContent className="max-h-[80vh] w-11/12 max-w-none overflow-scroll rounded-md bg-opacity-70 bg-gradient-to-b from-[#ffffff] to-blue-50 lg:w-2/3">
-        <DialogHeader>
-          <DialogTitle className="mb-4">Oefening 1</DialogTitle>
-          <h3 className="mb-4">
-            Hier komt een korte samenvatting van een veel langer verhaal.
+      <DialogContent className="max-h-[80vh] w-11/12 max-w-[800px] overflow-scroll rounded-md bg-opacity-70 bg-gradient-to-b from-[#ffffff] to-blue-50 lg:w-2/3">
+        <DialogHeader className="mb-2 lg:mb-3">
+          <DialogTitle className="mb-1 w-full text-center font-sans text-2xl font-semibold tracking-[0.1rem] text-[#333333] text-opacity-90 lg:text-3xl">
+            Balansspel
+          </DialogTitle>
+          <h3 className="w-full text-center font-sans text-sm tracking-[1px] text-[#333333] text-opacity-90 lg:text-lg lg:tracking-[0.1rem]">
+            Wat zijn de voor- en nadelen van gebruik?
           </h3>
-          <DialogDescription className="flex flex-col gap-2">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-              ac ornare enim, et tristique ex. Integer luctus porttitor lorem,
-              at lobortis erat. Vivamus vestibulum orci interdum, luctus augue
-              id, venenatis purus. Phasellus eget faucibus mi, eu suscipit ante.
-              Fusce eu vestibulum magna. Orci varius natoque penatibus et magnis
-              dis parturient montes, nascetur ridiculus mus. Nunc lacinia est
-              neque, in pellentesque lacus interdum ullamcorper. Aenean nec
-              tellus vel massa hendrerit posuere. Ut pellentesque vehicula nisl
-              et rutrum. Donec ac erat molestie, varius leo ut, hendrerit velit.
-              Praesent cursus eros nec maximus varius. Nam volutpat, odio et
-              viverra placerat, sapien risus tempus diam, id venenatis neque
-              arcu sit amet urna. Suspendisse vitae sollicitudin nisi. Phasellus
-              vel orci ut urna aliquet semper vitae nec augue.
-            </p>
-            <p>
-              Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
-              posuere cubilia curae; Proin interdum sagittis auctor. Morbi
-              lacinia massa et quam tincidunt dignissim. Donec lobortis erat at
-              tempus luctus. Nam ullamcorper finibus euismod. Proin imperdiet
-              maximus dui, ut ullamcorper erat porta a. Fusce vestibulum ante
-              cursus purus luctus laoreet. Integer sodales diam cursus lectus
-              viverra, tempus congue ligula pharetra. Duis id pretium diam, in
-              varius arcu.
-            </p>
-            <p>
-              Phasellus quis bibendum libero, eu finibus orci. Proin magna leo,
-              congue sed lacus quis, luctus maximus eros. Sed semper elit vel
-              sapien eleifend dapibus. Fusce tellus lectus, finibus in purus at,
-              aliquet varius nisl. Donec maximus, orci volutpat ultrices
-              gravida, lorem felis pulvinar nibh, id scelerisque arcu quam vel
-              tellus. Integer euismod leo quis diam tincidunt venenatis. Proin
-              rhoncus pretium lectus et varius. Vivamus est mi, lacinia nec arcu
-              a, interdum congue enim. Maecenas in eros suscipit, maximus metus
-              vel, egestas sapien. Mauris tincidunt, tortor quis mollis rutrum,
-              leo dui cursus tortor, a fermentum orci nisl quis nulla. Nullam
-              fringilla cursus fermentum. Suspendisse et volutpat augue.
-            </p>
-            <p>
-              Sed vehicula, tellus ac elementum ultricies, libero sem eleifend
-              ex, viverra pretium felis enim ac lacus. Integer eu nulla justo.
-              Sed quis tempus orci. Sed in accumsan mauris. Suspendisse placerat
-              tortor ac nulla dictum, sed euismod sapien egestas. Aenean
-              pharetra, dui ut maximus efficitur, nisi magna malesuada purus, et
-              mollis lectus mi ut lacus. Morbi quis orci ullamcorper, vehicula
-              ex mattis, porta quam. Mauris euismod, arcu eu vehicula efficitur,
-              ipsum lectus mattis tellus, eget tincidunt enim sapien id metus.
-              Duis neque elit, dictum at fermentum at, porta et risus. Nullam
-              dui erat, porta ut mauris a, rutrum consectetur ex. Sed porta,
-              elit ac ornare cursus, sem urna tincidunt purus, sed ultricies
-              lorem enim ac sem.
-            </p>
-            <p>
-              Duis egestas tellus ut euismod luctus. Class aptent taciti
-              sociosqu ad litora torquent per conubia nostra, per inceptos
-              himenaeos. Donec finibus vehicula neque, euismod auctor leo
-              vehicula dictum. Sed quis convallis lorem, consequat pellentesque
-              tellus. Class aptent taciti sociosqu ad litora torquent per
-              conubia nostra, per inceptos himenaeos. Ut condimentum
-              pellentesque arcu scelerisque malesuada. Donec in lacus sodales,
-              tincidunt odio fermentum, pretium orci. Donec vulputate, lorem non
-              vestibulum consequat, diam quam hendrerit est, quis ultrices leo
-              quam ac ipsum. Aenean egestas eu nunc non scelerisque. Nullam
-              commodo leo sit amet augue rutrum, facilisis elementum urna
-              elementum. Sed eu scelerisque lectus.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-              ac ornare enim, et tristique ex. Integer luctus porttitor lorem,
-              at lobortis erat. Vivamus vestibulum orci interdum, luctus augue
-              id, venenatis purus. Phasellus eget faucibus mi, eu suscipit ante.
-              Fusce eu vestibulum magna. Orci varius natoque penatibus et magnis
-              dis parturient montes, nascetur ridiculus mus. Nunc lacinia est
-              neque, in pellentesque lacus interdum ullamcorper. Aenean nec
-              tellus vel massa hendrerit posuere. Ut pellentesque vehicula nisl
-              et rutrum. Donec ac erat molestie, varius leo ut, hendrerit velit.
-              Praesent cursus eros nec maximus varius. Nam volutpat, odio et
-              viverra placerat, sapien risus tempus diam, id venenatis neque
-              arcu sit amet urna. Suspendisse vitae sollicitudin nisi. Phasellus
-              vel orci ut urna aliquet semper vitae nec augue.
-            </p>
-            <p>
-              Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
-              posuere cubilia curae; Proin interdum sagittis auctor. Morbi
-              lacinia massa et quam tincidunt dignissim. Donec lobortis erat at
-              tempus luctus. Nam ullamcorper finibus euismod. Proin imperdiet
-              maximus dui, ut ullamcorper erat porta a. Fusce vestibulum ante
-              cursus purus luctus laoreet. Integer sodales diam cursus lectus
-              viverra, tempus congue ligula pharetra. Duis id pretium diam, in
-              varius arcu.
-            </p>
-            <p>
-              Phasellus quis bibendum libero, eu finibus orci. Proin magna leo,
-              congue sed lacus quis, luctus maximus eros. Sed semper elit vel
-              sapien eleifend dapibus. Fusce tellus lectus, finibus in purus at,
-              aliquet varius nisl. Donec maximus, orci volutpat ultrices
-              gravida, lorem felis pulvinar nibh, id scelerisque arcu quam vel
-              tellus. Integer euismod leo quis diam tincidunt venenatis. Proin
-              rhoncus pretium lectus et varius. Vivamus est mi, lacinia nec arcu
-              a, interdum congue enim. Maecenas in eros suscipit, maximus metus
-              vel, egestas sapien. Mauris tincidunt, tortor quis mollis rutrum,
-              leo dui cursus tortor, a fermentum orci nisl quis nulla. Nullam
-              fringilla cursus fermentum. Suspendisse et volutpat augue.
-            </p>
-            <p>
-              Sed vehicula, tellus ac elementum ultricies, libero sem eleifend
-              ex, viverra pretium felis enim ac lacus. Integer eu nulla justo.
-              Sed quis tempus orci. Sed in accumsan mauris. Suspendisse placerat
-              tortor ac nulla dictum, sed euismod sapien egestas. Aenean
-              pharetra, dui ut maximus efficitur, nisi magna malesuada purus, et
-              mollis lectus mi ut lacus. Morbi quis orci ullamcorper, vehicula
-              ex mattis, porta quam. Mauris euismod, arcu eu vehicula efficitur,
-              ipsum lectus mattis tellus, eget tincidunt enim sapien id metus.
-              Duis neque elit, dictum at fermentum at, porta et risus. Nullam
-              dui erat, porta ut mauris a, rutrum consectetur ex. Sed porta,
-              elit ac ornare cursus, sem urna tincidunt purus, sed ultricies
-              lorem enim ac sem.
-            </p>
-            <p>
-              Duis egestas tellus ut euismod luctus. Class aptent taciti
-              sociosqu ad litora torquent per conubia nostra, per inceptos
-              himenaeos. Donec finibus vehicula neque, euismod auctor leo
-              vehicula dictum. Sed quis convallis lorem, consequat pellentesque
-              tellus. Class aptent taciti sociosqu ad litora torquent per
-              conubia nostra, per inceptos himenaeos. Ut condimentum
-              pellentesque arcu scelerisque malesuada. Donec in lacus sodales,
-              tincidunt odio fermentum, pretium orci. Donec vulputate, lorem non
-              vestibulum consequat, diam quam hendrerit est, quis ultrices leo
-              quam ac ipsum. Aenean egestas eu nunc non scelerisque. Nullam
-              commodo leo sit amet augue rutrum, facilisis elementum urna
-              elementum. Sed eu scelerisque lectus.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-              ac ornare enim, et tristique ex. Integer luctus porttitor lorem,
-              at lobortis erat. Vivamus vestibulum orci interdum, luctus augue
-              id, venenatis purus. Phasellus eget faucibus mi, eu suscipit ante.
-              Fusce eu vestibulum magna. Orci varius natoque penatibus et magnis
-              dis parturient montes, nascetur ridiculus mus. Nunc lacinia est
-              neque, in pellentesque lacus interdum ullamcorper. Aenean nec
-              tellus vel massa hendrerit posuere. Ut pellentesque vehicula nisl
-              et rutrum. Donec ac erat molestie, varius leo ut, hendrerit velit.
-              Praesent cursus eros nec maximus varius. Nam volutpat, odio et
-              viverra placerat, sapien risus tempus diam, id venenatis neque
-              arcu sit amet urna. Suspendisse vitae sollicitudin nisi. Phasellus
-              vel orci ut urna aliquet semper vitae nec augue.
-            </p>
-            <p>
-              Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
-              posuere cubilia curae; Proin interdum sagittis auctor. Morbi
-              lacinia massa et quam tincidunt dignissim. Donec lobortis erat at
-              tempus luctus. Nam ullamcorper finibus euismod. Proin imperdiet
-              maximus dui, ut ullamcorper erat porta a. Fusce vestibulum ante
-              cursus purus luctus laoreet. Integer sodales diam cursus lectus
-              viverra, tempus congue ligula pharetra. Duis id pretium diam, in
-              varius arcu.
-            </p>
-            <p>
-              Phasellus quis bibendum libero, eu finibus orci. Proin magna leo,
-              congue sed lacus quis, luctus maximus eros. Sed semper elit vel
-              sapien eleifend dapibus. Fusce tellus lectus, finibus in purus at,
-              aliquet varius nisl. Donec maximus, orci volutpat ultrices
-              gravida, lorem felis pulvinar nibh, id scelerisque arcu quam vel
-              tellus. Integer euismod leo quis diam tincidunt venenatis. Proin
-              rhoncus pretium lectus et varius. Vivamus est mi, lacinia nec arcu
-              a, interdum congue enim. Maecenas in eros suscipit, maximus metus
-              vel, egestas sapien. Mauris tincidunt, tortor quis mollis rutrum,
-              leo dui cursus tortor, a fermentum orci nisl quis nulla. Nullam
-              fringilla cursus fermentum. Suspendisse et volutpat augue.
-            </p>
-            <p>
-              Sed vehicula, tellus ac elementum ultricies, libero sem eleifend
-              ex, viverra pretium felis enim ac lacus. Integer eu nulla justo.
-              Sed quis tempus orci. Sed in accumsan mauris. Suspendisse placerat
-              tortor ac nulla dictum, sed euismod sapien egestas. Aenean
-              pharetra, dui ut maximus efficitur, nisi magna malesuada purus, et
-              mollis lectus mi ut lacus. Morbi quis orci ullamcorper, vehicula
-              ex mattis, porta quam. Mauris euismod, arcu eu vehicula efficitur,
-              ipsum lectus mattis tellus, eget tincidunt enim sapien id metus.
-              Duis neque elit, dictum at fermentum at, porta et risus. Nullam
-              dui erat, porta ut mauris a, rutrum consectetur ex. Sed porta,
-              elit ac ornare cursus, sem urna tincidunt purus, sed ultricies
-              lorem enim ac sem.
-            </p>
-            <p>
-              Duis egestas tellus ut euismod luctus. Class aptent taciti
-              sociosqu ad litora torquent per conubia nostra, per inceptos
-              himenaeos. Donec finibus vehicula neque, euismod auctor leo
-              vehicula dictum. Sed quis convallis lorem, consequat pellentesque
-              tellus. Class aptent taciti sociosqu ad litora torquent per
-              conubia nostra, per inceptos himenaeos. Ut condimentum
-              pellentesque arcu scelerisque malesuada. Donec in lacus sodales,
-              tincidunt odio fermentum, pretium orci. Donec vulputate, lorem non
-              vestibulum consequat, diam quam hendrerit est, quis ultrices leo
-              quam ac ipsum. Aenean egestas eu nunc non scelerisque. Nullam
-              commodo leo sit amet augue rutrum, facilisis elementum urna
-              elementum. Sed eu scelerisque lectus.
-            </p>
-          </DialogDescription>
         </DialogHeader>
+        <div className="mb-4 flex flex-col gap-6 font-sans text-sm tracking-[1px] text-[#333333] text-opacity-90 lg:text-base">
+          <DialogDescription className="font-sans text-sm tracking-[1px] text-[#333333] text-opacity-90 lg:text-base">
+            Hier kan je het voor- en nadelen balans maken. Dit is makkelijk te
+            doen op een whiteboard of een velletje papier. Je kan het als volgt
+            vormgeven:
+          </DialogDescription>
+          <div className="mb-4 flex w-full flex-col items-center justify-center">
+            <BalansTabel />
+          </div>
+          <DialogDescription className="font-sans text-sm tracking-[1px] text-[#333333] text-opacity-90 lg:text-base">
+            Als je dit zorgvuldig uitwerkt kan je daarna de lijsten als volgt
+            interpreteren:
+          </DialogDescription>
+          <ul className="flex flex-col gap-4 px-4">
+            <li className="list-item list-disc pl-4 font-sans text-sm tracking-[1px] text-[#333333] text-opacity-90 lg:text-base">
+              De voordelen lijst bestaat eigenlijk uit een opsomming van de
+              functies van het gebruik en leert je dus iets over wat je nog moet
+              organiseren of ontkrachten om een stoppoging van een grotere kans
+              van slagen te voorzien.
+            </li>
+            <li className="list-item list-disc pl-4 font-sans text-sm tracking-[1px] text-[#333333] text-opacity-90 lg:text-base">
+              De nadelen lijst bestaat uit een opsomming van wat het allemaal
+              oplevert om te stoppen. Dit gaat vaak over gezondheid en geld.
+            </li>
+          </ul>
+        </div>
       </DialogContent>
     </Dialog>
+  );
+}
+
+function BalansTabel() {
+  return (
+    <div className="relative mt-2 flex h-60 w-full flex-col items-center justify-center lg:my-6 lg:mb-4 lg:h-60 lg:scale-100 lg:px-6">
+      <table
+        className={`h-full w-full border-collapse border-2 border-black border-opacity-70 bg-slate-300 p-2 text-center font-sans text-base font-semibold tracking-wide text-[#333] text-opacity-90 lg:text-lg`}
+      >
+        <thead>
+          <tr className="border-2 border-black border-opacity-70">
+            <th className="border-2 border-black border-opacity-70 px-4 text-left">
+              Voordelen
+            </th>
+            <th className="border-2 border-black border-opacity-70 px-4 text-left">
+              Nadelen
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="border-2 border-black border-opacity-70">
+            <th className="border-2 border-black border-opacity-70 px-4 text-left">
+              1.
+            </th>
+            <th className="border-2 border-black border-opacity-70 px-4 text-left">
+              1.
+            </th>
+          </tr>
+          <tr className="border-2 border-black border-opacity-70">
+            <th className="border-2 border-black border-opacity-70 px-4 text-left">
+              2.
+            </th>
+            <th className="border-2 border-black border-opacity-70 px-4 text-left">
+              2.
+            </th>
+          </tr>
+          <tr className="border-2 border-black border-opacity-70">
+            <th className="border-2 border-black border-opacity-70 px-4 text-left">
+              3.
+            </th>
+            <th className="border-2 border-black border-opacity-70 px-4 text-left">
+              3.
+            </th>
+          </tr>
+          <tr className="border-2 border-black border-opacity-70">
+            <th className="border-2 border-black border-opacity-70 px-4 text-left">
+              4.
+            </th>
+            <th className="border-2 border-black border-opacity-70 px-4 text-left">
+              4.
+            </th>
+          </tr>
+          <tr className="border-2 border-black border-opacity-70">
+            <th className="border-2 border-black border-opacity-70 px-4 text-left">
+              5.
+            </th>
+            <th className="border-2 border-black border-opacity-70 px-4 text-left">
+              5.
+            </th>
+          </tr>
+          <tr className="border-2 border-black border-opacity-70">
+            <th className="border-2 border-black border-opacity-70 px-4 text-left">
+              6.
+            </th>
+            <th className="border-2 border-black border-opacity-70 px-4 text-left">
+              6.
+            </th>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 }
