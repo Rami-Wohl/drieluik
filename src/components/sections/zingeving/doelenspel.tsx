cc7a868c487233ab8ba3088f7a003be0b9b37257
Rami Wohl <rami.wohl@nos.nl>
2024-11-06 20:57:52 +0100

    add text for doelenspel

--- src/components/sections/zingeving/doelenspel.tsx
+++ src/components/sections/zingeving/doelenspel.tsx
@@ -8,6 +8,14 @@ import {
 } from "~/components/ui/dialog";
 import { GameContainer } from "./kernquadranten";
 
+function ListItem({ children }: { children: string }) {
+  return (
+    <li className="font-sans text-sm tracking-[1px] text-[#333333] text-opacity-90 lg:text-base">
+      {children}
+    </li>
+  );
+}
+
 export function Doelenspel() {
   return (
     <Dialog>
@@ -15,174 +23,59 @@ export function Doelenspel() {
         icon={<DoelenspelIcon height={72} width={72} />}
         title="Doelenspel"
       />
-      <DialogContent className="max-h-[80vh] w-11/12 max-w-none overflow-scroll rounded-md bg-opacity-70 bg-gradient-to-b from-[#ffffff] to-blue-50 lg:w-2/3">
-        <DialogHeader>
-          <DialogTitle className="mb-4">Oefening 1</DialogTitle>
-          <h3 className="mb-4">
-            Hier komt een korte samenvatting van een veel langer verhaal.
+      <DialogContent className="max-h-[80vh] w-11/12 max-w-[800px] overflow-scroll rounded-md bg-opacity-70 bg-gradient-to-b from-[#ffffff] to-blue-50 lg:w-2/3">
+        <DialogHeader className="mb-2 lg:mb-3">
+          <DialogTitle className="mb-1 w-full text-center font-sans text-2xl font-semibold tracking-[0.1rem] text-[#333333] text-opacity-90 lg:text-3xl">
+            Doelenspel
+          </DialogTitle>
+          <h3 className="w-full text-center font-sans text-sm tracking-[1px] text-[#333333] text-opacity-90 lg:text-lg lg:tracking-[0.1rem]">
+            Wat wil je graag allemaal doen met je leven?
           </h3>
-          <DialogDescription className="flex flex-col gap-2">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac
-            ornare enim, et tristique ex. Integer luctus porttitor lorem, at
-            lobortis erat. Vivamus vestibulum orci interdum, luctus augue id,
-            venenatis purus. Phasellus eget faucibus mi, eu suscipit ante. Fusce
-            eu vestibulum magna. Orci varius natoque penatibus et magnis dis
-            parturient montes, nascetur ridiculus mus. Nunc lacinia est neque,
-            in pellentesque lacus interdum ullamcorper. Aenean nec tellus vel
-            massa hendrerit posuere. Ut pellentesque vehicula nisl et rutrum.
-            Donec ac erat molestie, varius leo ut, hendrerit velit. Praesent
-            cursus eros nec maximus varius. Nam volutpat, odio et viverra
-            placerat, sapien risus tempus diam, id venenatis neque arcu sit amet
-            urna. Suspendisse vitae sollicitudin nisi. Phasellus vel orci ut
-            urna aliquet semper vitae nec augue. Vestibulum ante ipsum primis in
-            faucibus orci luctus et ultrices posuere cubilia curae; Proin
-            interdum sagittis auctor. Morbi lacinia massa et quam tincidunt
-            dignissim. Donec lobortis erat at tempus luctus. Nam ullamcorper
-            finibus euismod. Proin imperdiet maximus dui, ut ullamcorper erat
-            porta a. Fusce vestibulum ante cursus purus luctus laoreet. Integer
-            sodales diam cursus lectus viverra, tempus congue ligula pharetra.
-            Duis id pretium diam, in varius arcu. Phasellus quis bibendum
-            libero, eu finibus orci. Proin magna leo, congue sed lacus quis,
-            luctus maximus eros. Sed semper elit vel sapien eleifend dapibus.
-            Fusce tellus lectus, finibus in purus at, aliquet varius nisl. Donec
-            maximus, orci volutpat ultrices gravida, lorem felis pulvinar nibh,
-            id scelerisque arcu quam vel tellus. Integer euismod leo quis diam
-            tincidunt venenatis. Proin rhoncus pretium lectus et varius. Vivamus
-            est mi, lacinia nec arcu a, interdum congue enim. Maecenas in eros
-            suscipit, maximus metus vel, egestas sapien. Mauris tincidunt,
-            tortor quis mollis rutrum, leo dui cursus tortor, a fermentum orci
-            nisl quis nulla. Nullam fringilla cursus fermentum. Suspendisse et
-            volutpat augue. Sed vehicula, tellus ac elementum ultricies, libero
-            sem eleifend ex, viverra pretium felis enim ac lacus. Integer eu
-            nulla justo. Sed quis tempus orci. Sed in accumsan mauris.
-            Suspendisse placerat tortor ac nulla dictum, sed euismod sapien
-            egestas. Aenean pharetra, dui ut maximus efficitur, nisi magna
-            malesuada purus, et mollis lectus mi ut lacus. Morbi quis orci
-            ullamcorper, vehicula ex mattis, porta quam. Mauris euismod, arcu eu
-            vehicula efficitur, ipsum lectus mattis tellus, eget tincidunt enim
-            sapien id metus. Duis neque elit, dictum at fermentum at, porta et
-            risus. Nullam dui erat, porta ut mauris a, rutrum consectetur ex.
-            Sed porta, elit ac ornare cursus, sem urna tincidunt purus, sed
-            ultricies lorem enim ac sem. Duis egestas tellus ut euismod luctus.
-            Class aptent taciti sociosqu ad litora torquent per conubia nostra,
-            per inceptos himenaeos. Donec finibus vehicula neque, euismod auctor
-            leo vehicula dictum. Sed quis convallis lorem, consequat
-            pellentesque tellus. Class aptent taciti sociosqu ad litora torquent
-            per conubia nostra, per inceptos himenaeos. Ut condimentum
-            pellentesque arcu scelerisque malesuada. Donec in lacus sodales,
-            tincidunt odio fermentum, pretium orci. Donec vulputate, lorem non
-            vestibulum consequat, diam quam hendrerit est, quis ultrices leo
-            quam ac ipsum. Aenean egestas eu nunc non scelerisque. Nullam
-            commodo leo sit amet augue rutrum, facilisis elementum urna
-            elementum. Sed eu scelerisque lectus. Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit. Integer ac ornare enim, et tristique
-            ex. Integer luctus porttitor lorem, at lobortis erat. Vivamus
-            vestibulum orci interdum, luctus augue id, venenatis purus.
-            Phasellus eget faucibus mi, eu suscipit ante. Fusce eu vestibulum
-            magna. Orci varius natoque penatibus et magnis dis parturient
-            montes, nascetur ridiculus mus. Nunc lacinia est neque, in
-            pellentesque lacus interdum ullamcorper. Aenean nec tellus vel massa
-            hendrerit posuere. Ut pellentesque vehicula nisl et rutrum. Donec ac
-            erat molestie, varius leo ut, hendrerit velit. Praesent cursus eros
-            nec maximus varius. Nam volutpat, odio et viverra placerat, sapien
-            risus tempus diam, id venenatis neque arcu sit amet urna.
-            Suspendisse vitae sollicitudin nisi. Phasellus vel orci ut urna
-            aliquet semper vitae nec augue. Vestibulum ante ipsum primis in
-            faucibus orci luctus et ultrices posuere cubilia curae; Proin
-            interdum sagittis auctor. Morbi lacinia massa et quam tincidunt
-            dignissim. Donec lobortis erat at tempus luctus. Nam ullamcorper
-            finibus euismod. Proin imperdiet maximus dui, ut ullamcorper erat
-            porta a. Fusce vestibulum ante cursus purus luctus laoreet. Integer
-            sodales diam cursus lectus viverra, tempus congue ligula pharetra.
-            Duis id pretium diam, in varius arcu. Phasellus quis bibendum
-            libero, eu finibus orci. Proin magna leo, congue sed lacus quis,
-            luctus maximus eros. Sed semper elit vel sapien eleifend dapibus.
-            Fusce tellus lectus, finibus in purus at, aliquet varius nisl. Donec
-            maximus, orci volutpat ultrices gravida, lorem felis pulvinar nibh,
-            id scelerisque arcu quam vel tellus. Integer euismod leo quis diam
-            tincidunt venenatis. Proin rhoncus pretium lectus et varius. Vivamus
-            est mi, lacinia nec arcu a, interdum congue enim. Maecenas in eros
-            suscipit, maximus metus vel, egestas sapien. Mauris tincidunt,
-            tortor quis mollis rutrum, leo dui cursus tortor, a fermentum orci
-            nisl quis nulla. Nullam fringilla cursus fermentum. Suspendisse et
-            volutpat augue. Sed vehicula, tellus ac elementum ultricies, libero
-            sem eleifend ex, viverra pretium felis enim ac lacus. Integer eu
-            nulla justo. Sed quis tempus orci. Sed in accumsan mauris.
-            Suspendisse placerat tortor ac nulla dictum, sed euismod sapien
-            egestas. Aenean pharetra, dui ut maximus efficitur, nisi magna
-            malesuada purus, et mollis lectus mi ut lacus. Morbi quis orci
-            ullamcorper, vehicula ex mattis, porta quam. Mauris euismod, arcu eu
-            vehicula efficitur, ipsum lectus mattis tellus, eget tincidunt enim
-            sapien id metus. Duis neque elit, dictum at fermentum at, porta et
-            risus. Nullam dui erat, porta ut mauris a, rutrum consectetur ex.
-            Sed porta, elit ac ornare cursus, sem urna tincidunt purus, sed
-            ultricies lorem enim ac sem. Duis egestas tellus ut euismod luctus.
-            Class aptent taciti sociosqu ad litora torquent per conubia nostra,
-            per inceptos himenaeos. Donec finibus vehicula neque, euismod auctor
-            leo vehicula dictum. Sed quis convallis lorem, consequat
-            pellentesque tellus. Class aptent taciti sociosqu ad litora torquent
-            per conubia nostra, per inceptos himenaeos. Ut condimentum
-            pellentesque arcu scelerisque malesuada. Donec in lacus sodales,
-            tincidunt odio fermentum, pretium orci. Donec vulputate, lorem non
-            vestibulum consequat, diam quam hendrerit est, quis ultrices leo
-            quam ac ipsum. Aenean egestas eu nunc non scelerisque. Nullam
-            commodo leo sit amet augue rutrum, facilisis elementum urna
-            elementum. Sed eu scelerisque lectus. Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit. Integer ac ornare enim, et tristique
-            ex. Integer luctus porttitor lorem, at lobortis erat. Vivamus
-            vestibulum orci interdum, luctus augue id, venenatis purus.
-            Phasellus eget faucibus mi, eu suscipit ante. Fusce eu vestibulum
-            magna. Orci varius natoque penatibus et magnis dis parturient
-            montes, nascetur ridiculus mus. Nunc lacinia est neque, in
-            pellentesque lacus interdum ullamcorper. Aenean nec tellus vel massa
-            hendrerit posuere. Ut pellentesque vehicula nisl et rutrum. Donec ac
-            erat molestie, varius leo ut, hendrerit velit. Praesent cursus eros
-            nec maximus varius. Nam volutpat, odio et viverra placerat, sapien
-            risus tempus diam, id venenatis neque arcu sit amet urna.
-            Suspendisse vitae sollicitudin nisi. Phasellus vel orci ut urna
-            aliquet semper vitae nec augue. Vestibulum ante ipsum primis in
-            faucibus orci luctus et ultrices posuere cubilia curae; Proin
-            interdum sagittis auctor. Morbi lacinia massa et quam tincidunt
-            dignissim. Donec lobortis erat at tempus luctus. Nam ullamcorper
-            finibus euismod. Proin imperdiet maximus dui, ut ullamcorper erat
-            porta a. Fusce vestibulum ante cursus purus luctus laoreet. Integer
-            sodales diam cursus lectus viverra, tempus congue ligula pharetra.
-            Duis id pretium diam, in varius arcu. Phasellus quis bibendum
-            libero, eu finibus orci. Proin magna leo, congue sed lacus quis,
-            luctus maximus eros. Sed semper elit vel sapien eleifend dapibus.
-            Fusce tellus lectus, finibus in purus at, aliquet varius nisl. Donec
-            maximus, orci volutpat ultrices gravida, lorem felis pulvinar nibh,
-            id scelerisque arcu quam vel tellus. Integer euismod leo quis diam
-            tincidunt venenatis. Proin rhoncus pretium lectus et varius. Vivamus
-            est mi, lacinia nec arcu a, interdum congue enim. Maecenas in eros
-            suscipit, maximus metus vel, egestas sapien. Mauris tincidunt,
-            tortor quis mollis rutrum, leo dui cursus tortor, a fermentum orci
-            nisl quis nulla. Nullam fringilla cursus fermentum. Suspendisse et
-            volutpat augue. Sed vehicula, tellus ac elementum ultricies, libero
-            sem eleifend ex, viverra pretium felis enim ac lacus. Integer eu
-            nulla justo. Sed quis tempus orci. Sed in accumsan mauris.
-            Suspendisse placerat tortor ac nulla dictum, sed euismod sapien
-            egestas. Aenean pharetra, dui ut maximus efficitur, nisi magna
-            malesuada purus, et mollis lectus mi ut lacus. Morbi quis orci
-            ullamcorper, vehicula ex mattis, porta quam. Mauris euismod, arcu eu
-            vehicula efficitur, ipsum lectus mattis tellus, eget tincidunt enim
-            sapien id metus. Duis neque elit, dictum at fermentum at, porta et
-            risus. Nullam dui erat, porta ut mauris a, rutrum consectetur ex.
-            Sed porta, elit ac ornare cursus, sem urna tincidunt purus, sed
-            ultricies lorem enim ac sem. Duis egestas tellus ut euismod luctus.
-            Class aptent taciti sociosqu ad litora torquent per conubia nostra,
-            per inceptos himenaeos. Donec finibus vehicula neque, euismod auctor
-            leo vehicula dictum. Sed quis convallis lorem, consequat
-            pellentesque tellus. Class aptent taciti sociosqu ad litora torquent
-            per conubia nostra, per inceptos himenaeos. Ut condimentum
-            pellentesque arcu scelerisque malesuada. Donec in lacus sodales,
-            tincidunt odio fermentum, pretium orci. Donec vulputate, lorem non
-            vestibulum consequat, diam quam hendrerit est, quis ultrices leo
-            quam ac ipsum. Aenean egestas eu nunc non scelerisque. Nullam
-            commodo leo sit amet augue rutrum, facilisis elementum urna
-            elementum. Sed eu scelerisque lectus.
-          </DialogDescription>
         </DialogHeader>
+        <div className="mb-4 flex flex-col gap-6 font-sans text-sm tracking-[1px] text-[#333333] text-opacity-90 lg:text-base">
+          <DialogDescription className="flex flex-col gap-6 font-sans text-sm tracking-[1px] text-[#333333] text-opacity-90 lg:text-base">
+            Soms zal je je leven compleet nieuw vorm moeten geven als je uit een
+            verslaving komt. Het doelenspel kan helpen om in beeld te brengen
+            welke dingen je graag wil doen en hoe belangrijk die voor je zijn.
+          </DialogDescription>
+          <DialogDescription className="flex flex-col gap-6 font-sans text-sm tracking-[1px] text-[#333333] text-opacity-90 lg:text-base">
+            Je begint met zoveel mogelijk categorieën te definiëren. Je geeft je
+            cliënt dan 10 lege briefjes waar ze vervolgens doelen op mogen
+            schrijven. Moedig dan vooral aan om de doelen op zo veel mogelijk
+            verschillende categorieën te laten baseren.
+          </DialogDescription>
+          <div className="flex flex-col gap-2">
+            <span className="font-sans text-base font-medium tracking-[1px] text-[#333333] text-opacity-90 lg:text-lg">
+              Categoriën
+            </span>
+            <ol className="pl-1 font-sans text-sm tracking-[1px] text-[#333333] text-opacity-90 lg:text-base">
+              <ListItem>1. Behandeling</ListItem>
+              <ListItem>2. Wonen</ListItem>
+              <ListItem>3. Financiën</ListItem>
+              <ListItem>4. Hobby&apos;s</ListItem>
+              <ListItem>5. Werk</ListItem>
+              <ListItem>6. Opleiding</ListItem>
+              <ListItem>7. Familie</ListItem>
+              <ListItem>8. Sport</ListItem>
+              <ListItem>9. Voeding</ListItem>
+              <ListItem>10. Spiritualiteit</ListItem>
+            </ol>
+          </div>
+          <DialogDescription className="flex flex-col gap-6 font-sans text-sm tracking-[1px] text-[#333333] text-opacity-90 lg:text-base">
+            Vervolgens leg je alle doelen op een stapel rechts van je (de start
+            stapel). Dan pak je twee willekeurige doelen en die leg je recht
+            voor je neer, dit zijn dan de strijdende doelen. Laat deze strijd op
+            intuïtie gaan en denk er vooral niet te lang over na.
+          </DialogDescription>
+          <DialogDescription className="flex flex-col gap-6 font-sans text-sm tracking-[1px] text-[#333333] text-opacity-90 lg:text-base">
+            Het winnende doel blijft liggen en de verliezer leg je links van je
+            neer. Dit doe je tot er één winnaar voor je blijft liggen en alle
+            doelen op een stapel links van je liggen. Het winnende doel schuif
+            je dan naar boven (en daar schrijf je 1. op) en alle overige doelen
+            leg je weer rechts van je neer op de startstapel en je herhaalt
+            alles totdat er een rangorde in de doelen is ontstaan.
+          </DialogDescription>
+        </div>
       </DialogContent>
     </Dialog>
   );
