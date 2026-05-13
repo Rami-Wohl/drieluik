722d792e2736dc28b0093adc2f198dcc2b66e875
Rami Wohl <rami.wohl@nos.nl>
2026-05-13 12:45:06 +0200

    fix: layout again

--- src/components/nav/layout-with-nav.tsx
+++ src/components/nav/layout-with-nav.tsx
@@ -6,20 +6,22 @@ import { NavProvider } from "~/context/nav-context";
 
 export const LayoutWithNav = ({ children }: { children: ReactNode }) => {
   return (
-    <>
-      <NavProvider>
+    <NavProvider>
+      <div className="flex min-h-screen flex-col bg-gradient-to-b from-backgroundStart to-background">
         <NavComponents />
-        <main className="relative flex min-h-screen w-full flex-col items-center bg-gradient-to-b from-backgroundStart to-background pb-12 font-sans">
+
+        <main className="relative flex flex-1 flex-col items-center pb-12 font-sans">
           <div className="relative flex w-full max-w-7xl flex-col items-center">
             {children}
           </div>
         </main>
-      </NavProvider>
-      <footer className="flex h-10 w-full items-center justify-center border border-t border-black border-opacity-50">
-        <span className="font-sans text-sm">
-          Design en ontwikkeling door Rami Wohl © {new Date().getFullYear()}
-        </span>
-      </footer>
-    </>
+
+        <footer className="flex h-10 w-full items-center justify-center border-t border-black/50">
+          <span className="font-sans text-sm">
+            Design en ontwikkeling door Rami Wohl © {new Date().getFullYear()}
+          </span>
+        </footer>
+      </div>
+    </NavProvider>
   );
 };
