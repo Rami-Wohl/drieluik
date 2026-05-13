0ac09fa49969b21b919388a1fcb77e2c68d6e9f5
Rami Wohl <rami.wohl@nos.nl>
2026-05-13 12:31:54 +0200

    feat: sadly this is necessary

--- src/components/nav/layout-with-nav.tsx
+++ src/components/nav/layout-with-nav.tsx
@@ -9,12 +9,17 @@ export const LayoutWithNav = ({ children }: { children: ReactNode }) => {
     <>
       <NavProvider>
         <NavComponents />
-        <main className="relative flex min-h-screen w-full flex-col items-center bg-gradient-to-b from-backgroundStart to-background pb-12 font-sans">
+        <main className="relative flex w-full flex-col items-center bg-gradient-to-b from-backgroundStart to-background pb-12 font-sans">
           <div className="relative flex w-full max-w-7xl flex-col items-center">
             {children}
           </div>
         </main>
       </NavProvider>
+      <footer className="flex h-10 w-full items-center justify-center border border-t border-black border-opacity-50">
+        <span className="font-sans text-sm">
+          Design en ontwikkeling door Rami Wohl © {new Date().getFullYear()}
+        </span>
+      </footer>
     </>
   );
 };
