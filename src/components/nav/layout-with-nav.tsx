fa92f092e6fb774afcbf131763e0378f009e40c5
Rami Wohl <rami.wohl@nos.nl>
2026-05-13 12:34:11 +0200

    fix: layout

--- src/components/nav/layout-with-nav.tsx
+++ src/components/nav/layout-with-nav.tsx
@@ -9,7 +9,7 @@ export const LayoutWithNav = ({ children }: { children: ReactNode }) => {
     <>
       <NavProvider>
         <NavComponents />
-        <main className="relative flex w-full flex-col items-center bg-gradient-to-b from-backgroundStart to-background pb-12 font-sans">
+        <main className="relative flex min-h-screen w-full flex-col items-center bg-gradient-to-b from-backgroundStart to-background pb-12 font-sans">
           <div className="relative flex w-full max-w-7xl flex-col items-center">
             {children}
           </div>
